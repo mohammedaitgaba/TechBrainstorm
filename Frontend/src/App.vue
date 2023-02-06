6e69b6faa66e3bec103367bbc10776ed5d8d4118
mohammedaitgaba <93977101+mohammedaitgaba@users.noreply.github.com>
2023-02-06 18:35:11 +0100

    add vue js for frontend using Ts

---
+++ Frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="starter">
+    <h1>Welcome to :</h1>
+    <h1>TechBrainstorm</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+  .starter{
+    text-align: center;
+  }
+</style>
